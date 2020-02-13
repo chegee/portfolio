4549fd8a4d9d44a520f434be5464a403cdb130c1
chegee <yogachara@gmail.com>
2020-02-13 03:51:35 +0300

    efp and st

--- js/fm.revealator.jquery.js
+++ js/fm.revealator.jquery.js
@@ -35,7 +35,7 @@ $(function () {
 		timer:           null,
 		busy:            false,
 		scroll_padding:  0,
-		effects_padding: 0,
+		effects_padding: -200,
 		refresh:         function () {}
 	}, typeof Revealator !== 'undefined' ? Revealator : {});
 
